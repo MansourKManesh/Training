25858a0a49c344060c527fdae1b434b2b0f858dc
Mansour <Mansour.Manesh@gmail.com>
2025-03-17 03:13:40 -0700

    بانک داده ها

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 app = Flask(__name__)
 
+# تنظیمات اتصال به MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flaskuser:A1Alaki@498@localhost/myflaskdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# مقداردهی اولیه به دیتابیس
+from app.models import db, User
+db.init_app(app)
+
 @app.route('/')
-def homepage():
-    return render_template('index.html')
+def home():
+    return "اتصال Flask به MySQL موفقیت‌آمیز بود!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
